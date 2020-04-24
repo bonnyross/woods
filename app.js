b1cb7e84748d3bdbb385d95ab8208baa6041f25b
bonnyross <daross@gmx.co.uk>
2020-04-24 14:37:27 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+// This framework was created by the Express package
+// The logger has been commented out due to its generating GET messages to the console.log
+// 
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+//var logger = require('morgan');
 //var favicon 		= require('serve-favicon');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,11 +17,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'))
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
