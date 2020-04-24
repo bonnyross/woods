0e534927e04f755c1862fca782a9357fcc0f99e7
bonnyross <daross@gmx.co.uk>
2020-04-24 16:02:47 +0100

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,7 @@
-var express = require('express');
-var router = express.Router();
-var fs 		= require('fs');
+var express 	= require('express');
+var router 		= express.Router();
+var fs 				= require('fs');
 
-/*router.get('/woodland/map', function(req, res, next) {
-	var filename = "./public/topography_1.jpg";
-	fs.readFile(filename, function (err, data) {
-		if (err) {console.log("Could not open topography"+ err); } 
-		else { res.send( data ); };});});*/
-		
 router.get('/woodland/woods', function(request, res) {
 	var filename = "./public/geojson/woods.geojson";
     fs.readFile(filename, function (err, data) {
@@ -20,12 +14,6 @@ router.get('/woodland/parkland', function(request, res) {
 		if (err) {console.log("Could not open parkland"+ err);} 
 		else { res.send( data ); };	}); });
 				
-router.get('/woodland/settlement', function(request, res) {
-	var filename = "./public/geojson/settlement.geojson";
-    fs.readFile(filename, function (err, data) {
-		if (err) {console.log("Could not open settlement"+ err);} 
-		else { res.send( data ); };	}); });
-		
 router.get('/woodland/area', function(request, res) {
 	var filename = "./public/geojson/area.geojson";
     fs.readFile(filename, function (err, data) {
@@ -38,12 +26,6 @@ router.get('/woodland/boundary', function(request, res) {
 		if (err) {console.log("Could not open boundary"+ err);} 
 		else { res.send( data ); };	}); });		
 
-/*router.get('/woodland/areas', function(request, res) {
-	var filename = "./public/pdfs/roads.geojson";
-    fs.readFile(filename, function (err, data) {
-		if (err) {console.log("Could not open pdf"+ err);} 
-		else { res.send( data ); };	}); });*/
-		
 router.get('/woodland/roads', function(request, res) {
 	var filename = "./public/geojson/roads.geojson";
     fs.readFile(filename, function (err, data) {
